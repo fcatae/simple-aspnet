088857eff7149d1b5263f3c4e04729e5697ee87d
logonrm <logonrm@fiap.com.br>
2018-03-27 21:29:29 -0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                        .UseKestrel()
+                        .Configure(MyChat)
+                        .Build();
+
+            host.Run();
+        }
+            
+        static void MyChat(IApplicationBuilder app)
+        {
+            app.Run( async ctx => {
+                await ctx.Response.WriteAsync("Boa noite");
+            });
+        }
+    }
+}
